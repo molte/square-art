c4c4827ffd47ae478c018e7933e03358c1c5cc0b
Molte Emil Strange Andersen <molte93@gmail.com>
2017-11-17 18:15:17 +0100

    Cleanup and documentation

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// 16x16:AABEEYIgMAaICCgKQiEUFEIhKAqICDAGgiBEEQAAAAA=
-
+// Number of bits per byte
 const BYTE_SIZE = 8;
 
+// An array of booleans
 class BitArray {
+  // Initialises a new instance of BitArray. If input is a positive number, the
+  // BitArray will have this number of elements. If input is a Uint8Array, the
+  // underlying ArrayBuffer will be used to store the contents of the BitArray.
   constructor(input) {
     if (typeof input == 'number') {
       this.length = input;
@@ -14,6 +17,10 @@ class BitArray {
     }
   }
   
+  // Gets and sets, respectively, the number of bytes in the BitArray. Notice
+  // that running
+  //   bitArray.bytes = bitArray.bytes;
+  // may actually change the length of the array.
   get bytes() {
     return Math.ceil(this.length / BYTE_SIZE);
   }
@@ -21,43 +28,57 @@ class BitArray {
     this.length = BYTE_SIZE * n;
   }
   
+  // Creates a new ArrayBuffer (populated with zeros) to store the contents of
+  // the BitArray.
   resetBuffer() {
     this.setBuffer(new ArrayBuffer(this.bytes));
   }
   
+  // Instructs the BitArrayt to use the given ArrayBuffer to store its contents.
   setBuffer(buffer) {
     this.buffer = buffer;
     this.uint8View = new Uint8Array(this.buffer);
   }
   
+  // Returns the value of the BitArray at the given index. The return value is a
+  // boolean.
   get(bitIndex) {
     var byteIndex = Math.floor(bitIndex / BYTE_SIZE);
     var byte = this.uint8View[byteIndex];
     return (byte >> bitIndex - byteIndex * BYTE_SIZE & 1) > 0;
   }
   
+  // Toggles the state of the value at the given index between true and false.
   toggle(bitIndex) {
     var byteIndex = Math.floor(bitIndex / BYTE_SIZE);
     var comp = 1 << (bitIndex - byteIndex * BYTE_SIZE);
     this.uint8View[byteIndex] ^= comp;
   }
   
+  // Returns a new BitArray with the same data, but compressed with the DEFLATE
+  // algorithm.
   compressed() {
     return new BitArray(pako.deflateRaw(this.uint8View));
   }
   
+  // Assuming the current BitArray has been compressed with the 'compressed'
+  // method, returns a new BitArray with the uncompressed data.
   uncompressed() {
     return new BitArray(pako.inflateRaw(this.uint8View));
   }
   
+  // Returns a base64 string representation of the BitArray data.
   toBase64() {
     return btoa(this.uint8View.reduce((data, byte) => data + String.fromCharCode(byte), ''));
   }
   
+  // Returns the base64 string representation of the compressed BitArray.
   toCompressedBase64() {
     return this.compressed().toBase64();
   }
   
+  // Initialises a new BitArray whose data is representable as the given
+  // base64-encoded string.
   static fromBase64(base64) {
     var binary = atob(base64);
     var ba = new BitArray(BYTE_SIZE * binary.length);
@@ -67,42 +88,35 @@ class BitArray {
     return ba;
   }
   
+  // Initialises a new BitArray whose data -- in compressed form -- is
+  // representable as the given base64-encoded string.
   static fromCompressedBase64(base64) {
     return BitArray.fromBase64(base64).uncompressed();
   }
 }
 
-// function uint8ToBase64(uint8Array) {
-//   return btoa(uint8Array.reduce((data, byte) => data + String.fromCharCode(byte), ''));
-// }
-
-// function base64ToUint8(base64) {
-//   var binary = atob(base64);
-//   var uint8Array = new Uint8Array(binary.length);
-//   for (var k = 0; k < binary.length; k++) {
-//     uint8Array[k] = binary.charCodeAt(k);
-//   }
-//   return uint8Array;
-// }
-
+// Define shortcuts for DOM elements
 var cursorPosContainerElement = document.getElementById('cursor-pos');
 var cursorPosXElement = document.getElementById('cursor-pos-x');
 var cursorPosYElement = document.getElementById('cursor-pos-y');
+var canvasElement = document.getElementById('drawing-board');
 
-var canvasElement = document.getElementById("drawing-board");
+// Drawing context for the canvas
 var context = canvasElement.getContext('2d');
 
+// Position coordinates of the canvas element on the webpage
 var canvasPosition = [canvasElement.offsetLeft, canvasElement.offsetTop];
 
-var size = 32; // number of boxes
-var padding = canvasElement.width / size; // pixels per box
+// Number of rows and columns of squares
+var size = 32;
+
+// Pixels per square
+var padding = canvasElement.width / size;
+
+// Storage container for the pattern drawn on the canvas
 var patttern;
 
-// var pattern = new Array(size * size); // memory
-// for (var k = 0; k < pattern.length; k++) {
-//   pattern[k] = false;
-// }
-
+// Draws a grid of helper lines separating the squares
 function drawGrid() {
   context.beginPath();
   context.strokeStyle = '#999';
@@ -118,53 +132,26 @@ function drawGrid() {
   context.stroke();
 }
 
+// Fills the square in column x and row y with colour.
 function fillSquare(x, y) {
   context.beginPath();
   context.fillStyle = '#333';
   context.fillRect(x * padding, y * padding, padding, padding);
 }
 
-// function clearSquare(x, y) {
-//   context.clearRect(x * padding, y * padding, padding, padding);
-// }
-
-function drawSquare(x, y, fill) {
-  context.beginPath();
-  context.fillStyle = fill ? '#333' : 'white';
-  context.fillRect(x * padding, y * padding, padding, padding);
-  
-  // context.beginPath();
-  // context.lineJoin = 'miter';
-  // context.lineCap = 'square';
-  // context.moveTo(x * padding, y * padding);
-  // context.lineTo((x + 1) * padding, y * padding);
-  // context.lineTo((x + 1) * padding, (y + 1) * padding);
-  // context.lineTo(x * padding, (y + 1) * padding);
-  // context.closePath();
-  // context.stroke();
-  
-  // if (fill) {
-  //   drawSquare(x, y);
-  // } else {
-  //   clearSquare(x, y);
-  // }
-}
-
+// Switches the state of the boolean representing the square in column x and row
+// y. The method does not update the canvas to reflect this change.
 function toggleSquare(x, y) {
   var i = x + y * size;
-  // var z = !pattern[i];
-  // // drawSquare(x, y, z);
-  // pattern[i] = z;
   pattern.toggle(i);
 }
 
+// Clears the canvas and draws the pattern and helper grid anew.
 function redrawCanvas() {
   context.clearRect(0, 0, size * padding, size * padding);
   
   for (var x = 0; x < size; x++) {
     for (var y = 0; y < size; y++) {
-      // drawSquare(x, y, pattern[x + y * size]);
-      // drawSquare(x, y, pattern.get(x + y * size));
       if (pattern.get(x + y * size)) {
         fillSquare(x, y);
       }
@@ -174,30 +161,8 @@ function redrawCanvas() {
   drawGrid();
 }
 
-function getHexPattern() {
-  var chunkSize = 5;
-  var hex = '';
-  
-  var n, k, l, i;
-  for (k = 0; k < pattern.length / chunkSize; k++) {
-    n = 0;
-    
-    for (l = 0; l < chunkSize; l++) {
-      i = k * chunkSize + l;
-      if (i >= pattern.length) break;
-      
-      // if (pattern[i]) {
-      if (pattern.get(i)) {
-        n |= (1 << l);
-      }
-    }
-    
-    hex = n.toString(Math.pow(2, chunkSize)) + hex;
-  }
-  
-  return hex;
-}
-
+// Reads the URL hash and updates the drawing pattern if the hash contains an
+// encoded pattern.
 function updatePatternFromHash() {
   var hash = window.location.hash;    
   if (hash && hash.length > 1) {
@@ -207,23 +172,29 @@ function updatePatternFromHash() {
   }
 }
 
+// Returns the position of the cursor in units of squares at the time the given
+// event was fired.
 function computeCursorPosition(event) {
   var x = Math.floor((event.pageX - canvasPosition[0]) / padding);
   var y = Math.floor((event.pageY - canvasPosition[1]) / padding);
   return [x, y];
 }
 
+// Toggle the filling of the square the user clicks
 canvasElement.addEventListener('click', function(event) {
   var x, y;
   [x, y] = computeCursorPosition(event);
   
   toggleSquare(x, y);
   redrawCanvas();
+  
+  // Remember the previous pattern if the user clicks the browser back button
   window.history.pushState(pattern.buffer, '', '#' + pattern.toCompressedBase64());
   
   event.preventDefault();
 });
 
+// Update the cursor position indicator when the cursor moves inside the canvas
 canvasElement.addEventListener('mousemove', function(event) {
   var x, y;
   [x, y] = computeCursorPosition(event);
@@ -232,21 +203,25 @@ canvasElement.addEventListener('mousemove', function(event) {
   cursorPosYElement.innerText = y;
 });
 
+// Only show the cursor position when hovering over the canvas
 canvasElement.addEventListener('mouseenter', function(event) {
   cursorPosContainerElement.className = '';
 });
-
 canvasElement.addEventListener('mouseleave', function(event) {
   cursorPosContainerElement.className = 'hidden';
 });
 
+// Clear canvas when asked
 document.getElementById('reset-canvas').addEventListener('click', function(event) {
   pattern.resetBuffer();
   redrawCanvas();
   window.history.pushState(null, '', '#');
 });
 
+// Monitor when user clicks the brower back button (or similar)
 window.addEventListener('popstate', function(event) {
+  // If a pattern state is saved, use this; otherwise read off the pattern (if
+  // it is there) from the URL hash
   if (event.state) {
     pattern.setBuffer(event.state);
   } else {
@@ -255,6 +230,6 @@ window.addEventListener('popstate', function(event) {
   redrawCanvas();
 });
 
+// Read pattern from URL hash and prepare canvas on pageload
 updatePatternFromHash();
 redrawCanvas();
-
